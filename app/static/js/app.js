7693d8dd0cbe0967862bdc485c4dc14447b4508f
Taje Carter <taje.carter@mymona.uwi.edu>
2017-04-04 13:58:11 +0000

    Ex 3

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,1 +1,7 @@
 // Your JavaScript Code here
+var app = angular.module('myApp', []);
+ app.controller('myCtrl', function($scope, $http) {
+     $http.get("/api/thumbnails").then(function(response) {
+         $scope.myData = response.data.thumbnails;
+    });
+ });
